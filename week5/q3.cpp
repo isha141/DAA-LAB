8357ac31113363dd025ffe29e91e73d7bb661f99
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2022-05-24 07:52:54 +0530

    Create q3.cpp

---
+++ week5/q3.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n,m;
+   cout<<"Enter the number of vertices and edges "<<endl;
+   cin>>n>>m;   
+   int adj[n+1][n+1]={0};
+   for(int i=1;i<=m;i++)
+   {
+      int u,v;
+      cin>>u>>v;
+     adj[u][v]=1;
+     adj[v][u]=1;
+   }
+   for(int i=1;i<=n;i++)
+   {
+      for(int j=1;j<=n;j++)
+      {
+         cout<<adj[i][j]<<" ";
+      }
+      cout<<endl;
+   }
+
+
+}
